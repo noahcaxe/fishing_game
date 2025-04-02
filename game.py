d32791dc2807e05da9fe5b730f335d17e02925d6
Nazar <2015VMr.Nazar@gmail.com>
2025-04-02 18:46:20 +0200

    Created Window settings.

--- game.py
+++ game.py
@@ -2,3 +2,12 @@ import pygame
 import random
 
 pygame.init()
+
+#create a window
+
+window_width = 800 
+window_height = 600 
+window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Fishing Game")
+
+# colors
