eefd93754e00df71becc5ef2ca49bdab4cdd1dc7
Nazar <2015VMr.Nazar@gmail.com>
2025-04-02 19:23:43 +0200

    Added downloading images and game variables

--- game.py
+++ game.py
@@ -11,3 +11,20 @@ window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Fishing Game")
 
 # colors
+WHITE = (255, 255, 255)
+
+#game variables
+player_x = window_width // 2
+player_y = window_height - 100
+player_width = 50
+player_height = 50
+
+#Download images 
+background_image = pygame.image.load("beach_background.png")
+background_image = pygame.transform.scale(background_image, (window_width, window_height))
+player_img = pygame.image.load("stickman.png")
+player_img = pygame.transform.scale(player_img, (player_width, player_height))
+rod_img = pygame.image.load("rod.png")
+rod_img = pygame.transform.scale(rod_img, (100, 10))
+fish_img = pygame.image.load("fish.png")
+fish_img = pygame.transform.scale(fish_img, (40, 30))
